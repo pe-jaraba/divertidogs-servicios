4eeb999307bc95faa811a088f3228955e8d9fabf
pedro <pejaraba@yseop.com>
2022-06-22 13:15:35 -0500

    chore(init): [DOG-15] springboot boilerplate

---
+++ src/main/kotlin/com/divertidogs/divertidogsservicios/DivertidogsServiciosApplication.kt
@@ -0,0 +1,11 @@
+package com.divertidogs.divertidogsservicios
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DivertidogsServiciosApplication
+
+fun main(args: Array<String>) {
+	runApplication<DivertidogsServiciosApplication>(*args)
+}
---
+++ src/test/kotlin/com/divertidogs/divertidogsservicios/DivertidogsServiciosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.divertidogs.divertidogsservicios
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DivertidogsServiciosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
